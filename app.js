3049e8a77d67c2b9381393373a1fc59fb6e5be7d
unknown <pathanadnan729@gmail.com>
2025-10-28 19:14:26 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const tableRoutes = require('./routes/tableRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const menuRoutes = require('./routes/menuRoutes');
+const chefRoutes = require('./routes/chefRoutes');
+const analyticsRoutes = require('./routes/analyticsRoutes');
+const clientRoutes = require('./routes/clientRoutes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/tables', tableRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/menu', menuRoutes);
+app.use('/api/chefs', chefRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/clients', clientRoutes);
+
+module.exports = app;
